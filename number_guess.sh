5d4025b8feb02a519d175a9bed67daeee663f8d1
Zoro <bathrizmail@gmail.com>
2025-03-02 18:31:26 +0000

    feat:Game function added

--- number_guess.sh
+++ number_guess.sh
@@ -4,6 +4,33 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+GAME() {
+  RANDOM_NUMBER=$(( $RANDOM % 1000 + 1 ))
+
+  echo $RANDOM_NUMBER
+
+  echo -e "\nGuess the secret number between 1 and 1000:"
+
+  read NUMBER_GUESS
+
+  until (( $NUMBER_GUESS == RANDOM_NUMBER ))
+  do
+    if [[ ! $NUMBER_GUESS =~ ^[0-9]+$ ]]
+    then
+      echo -e "\nThat is not an integer, guess again:"
+    elif [[ $NUMBER_GUESS -gt $RANDOM_NUMBER ]]
+    then
+      echo -e "\nIt's lower than that, guess again:"
+    elif [[ $NUMBER_GUESS -lt $RANDOM_NUMBER ]]
+    then
+      echo -e "\nIt's higher than that, guess again:"
+    fi
+    read NUMBER_GUESS
+  done
+
+  echo -e "\nYou guessed it in <number_of_guesses> tries. The secret number was $RANDOM_NUMBER. Nice job!"
+}
+
 echo -e "\n~~ Number Guessing Game ~~\n"
 
 echo enter your username:
@@ -21,6 +48,8 @@ then
 else
   echo "$($PSQL "SELECT games_played, best_game FROM users WHERE user_id = $USER_ID ;")" | while IFS="|" read GAMES_PLAYED BEST_GAME
   do
-    echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
+    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   done
 fi
+
+GAME
